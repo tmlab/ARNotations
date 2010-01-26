a1b3ed0a85af9ae985f92b32ed66d9f18f70a21f
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-01-26 22:18:12 +0100

    Added spec stubs

--- spec/ar_notations_spec.rb
+++ spec/ar_notations_spec.rb
@@ -1,1 +1,47 @@
 require File.dirname(__FILE__) + '/spec_helper'
+
+describe ActiveRecord::Base do
+
+  describe "has_psi" do
+
+    it "allows to add a specific Subject-Identifiers as type for a Topic"
+
+  end
+
+  describe "has_topicmaps" do
+
+    it "allows binding a Topic to some specifig topicmaps"
+
+  end
+
+  describe "has_item_identifiers" do
+
+    it "allows arnotating the class attributes to use as ItemIdentifiers for the topic"
+
+  end
+
+  describe "has_subject_identifiers" do
+
+    it "allows arnotating the class attributes to use as SubjectIdentifiers for the topic"
+
+  end
+
+  describe "has_names" do
+
+    it "allows arnotating the class attributes to use as Names for the topic"
+
+  end
+
+  describe "has_occurrences" do
+
+    it "allows arnotating the class attributes to use as Occurrences for the topic"
+
+  end
+
+  describe "has_associations" do
+
+    it "allows arnotating the class attributes to use as Associations for the topic"
+
+  end
+
+end
