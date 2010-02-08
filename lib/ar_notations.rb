4182156c9ea14ec708c6dc27a064607f0858d20b
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-08 21:32:12 +0100

    Refactoring: Dateinamen klein

--- lib/ar_notations.rb
+++ lib/ar_notations.rb
@@ -2,8 +2,8 @@
 Mime::Type.register "application/xtm+xml", :xtm2
 
 require "ar_notations/TOXTM2"
-require "ar_notations/Characteristics"
-require "ar_notations/Associations"
+require "ar_notations/characteristics"
+require "ar_notations/associations"
 
 
 require "ar_notations/controller_core_ext"
