7487598b9ec55453986b698ebae1882fc2681709
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-08 22:17:07 +0100

    Specs fuer Associations um stubs erweitert

---
+++ spec/ar_notations/associations_spec.rb
@@ -0,0 +1,16 @@
+require 'ar_notations_spec_helper'
+
+describe ARNotations::Associations do
+  
+  describe "associations_to_xtm2" do
+    
+    it "should return an xtm2 representation of all associations"
+    
+  end
+  
+  describe "association_role_to_xtm2" do
+    
+    it "should return an xtm2 representation of the given association role"
+  end
+  
+end
