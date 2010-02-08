c164668e28a3942499f0b255b934b745fbc38515
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-08 22:17:33 +0100

    Specs fuer Characteristics um Stubs erweitert

---
+++ spec/ar_notations/characteristics_spec.rb
@@ -0,0 +1,23 @@
+require 'ar_notations_spec_helper'
+
+describe ARNotations::Characteristics do
+  
+  describe "default_name_to_xtm2" do
+    
+    it "should return an xtm2 representation of the default_name"
+    
+  end
+  
+  describe "name_to_xtm2" do
+    
+    it "should return an xtm2 representation of the name"
+    
+  end
+    
+  describe "occurrence_to_xtm2" do
+    
+    it "should return an xtm2 representation of the occurrence"
+  
+  end
+  
+end
