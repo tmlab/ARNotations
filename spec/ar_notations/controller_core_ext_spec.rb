dd8bd0d93a5375422e4b8716897151327b038abb
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-08 22:17:55 +0100

    Specs fuer Controller mit Stubs

---
+++ spec/ar_notations/controller_core_ext_spec.rb
@@ -0,0 +1,11 @@
+require 'ar_notations_spec_helper'
+
+describe ActionController::Base do
+
+  describe "array_to_xtm2" do
+    
+    it "should return an xtm2 representation of alle topics in the array"
+    
+  end
+  
+end
