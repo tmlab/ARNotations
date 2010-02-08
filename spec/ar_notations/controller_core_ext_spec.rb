1921558bc4aadbc8fca0aee6b06cf0d7e0e58c5f
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-08 23:05:19 +0100

    Stubs fuer Methoden

--- spec/ar_notations/controller_core_ext_spec.rb
+++ spec/ar_notations/controller_core_ext_spec.rb
@@ -4,7 +4,14 @@ describe ActionController::Base do
 
   describe "array_to_xtm2" do
     
-    it "should return an xtm2 representation of alle topics in the array"
+    it "should return an xtm2 representation of alle topics in the array" do
+      pending 
+      ProjectsController.new.array_to_xtm2(Project.all).should_not be_nil
+    end
+
+    it "should return nothing if the array is empty" do
+      ProjectsController.new.array_to_xtm2([]).should be_nil
+    end
     
   end
   
