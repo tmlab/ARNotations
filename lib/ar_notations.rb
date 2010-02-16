e5c695e90314ff4da059c146518c656a65392dc2
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-16 12:50:35 +0100

    Umstellung auf Array Ausgabe

--- lib/ar_notations.rb
+++ lib/ar_notations.rb
@@ -9,7 +9,7 @@ require "ar_notations/characteristics"
 require "ar_notations/associations"
 
 
-require "ar_notations/controller_core_ext"
-require "ar_notations/model_core_ext" 
+require "ar_notations/model_core_ext"
+require "ar_notations/array_ext" 
 
  
--- spec/ar_notations/controller_core_ext_spec.rb
+++ spec/ar_notations/controller_core_ext_spec.rb
@@ -1,16 +1,16 @@
 require 'ar_notations_spec_helper'
 
-describe ActionController::Base do
+describe Array do
 
-  describe "array_to_xtm2" do
+  describe "to_xtm2" do
     
     it "should return an xtm2 representation of alle topics in the array" do
       pending 
-      ProjectsController.new.array_to_xtm2(Project.all).should_not be_nil
+      Project.all.to_xtm2.should_not be_nil
     end
 
     it "should return nothing if the array is empty" do
-      ProjectsController.new.array_to_xtm2([]).should be_nil
+      [].to_xtm2.should be_nil
     end
     
   end
