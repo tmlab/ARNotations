73e4ded3ca4f3ba5fa9c7e95d16a95e12b06ea8e
Daniel Exner <exner@informatik.uni-leipzig.de>
2010-02-17 16:59:16 +0100

    Funktion um vernünftige Namen bzw. Namen Nodes zu bekommen

--- lib/ar_notations/id.rb
+++ lib/ar_notations/id.rb
@@ -10,8 +10,41 @@ module ARNotations
     end
 
     def get_psi
-      return absolute_identifier.sub(self.identifier, "")
+      if psi.blank?
+        return absolute_identifier.sub(self.identifier, "")
+      else
+        return psi
+      end
     end
 
+    def get_name(topic = self)
+      if topic.default_name.blank?
+        if topic.names.blank?
+          return topic.identifier
+        else
+          return topic.send("#{topic.names.first.at(0)}")
+        end
+      else
+        return topic.send("#{topic.default_name}")
+      end
+
+    end
+
+    def get_name_node(topic = self)
+
+      if topic.default_name.blank?
+        if topic.names.blank?
+          name = REXML::Element.new 'name'
+          name << TOXTM2.value(topic.identifier)
+        else
+          n_attr = topic.names.first
+          name = name_to_xtm2(n_attr.at(0), n_attr.at(1), topic)
+        end
+      else
+        name = default_name_to_xtm2(topic.default_name, topic)
+      end
+
+      return name
+    end
   end
 end
