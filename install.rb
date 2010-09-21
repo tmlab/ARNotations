8a5e1cc9b57e012f2cbac6603b97e775653356dc
darkwingdex@googlemail.com <darkwingdex@googlemail.com>
2010-09-21 17:50:19 +0200

    + added _initial_ Copyright notice

--- install.rb
+++ install.rb
@@ -1,1 +1,9 @@
+# ARNotations is a Ruby on Rails plugin to map any kind of ActiveRecord
+# model to a Topic Map Fragment, providing the ability
+# to expose them TMExplore compatible using REST.
+#
+# Author:: Daniel Exner
+# Copyright:: Copyright (c) 2010 Daniel Exner
+# License:: MIT License (http://www.opensource.org/licenses/mit-license.php)
+
 # Install hook code here
--- lib/ar_notations/characteristics.rb
+++ lib/ar_notations/characteristics.rb
@@ -1,3 +1,11 @@
+# ARNotations is a Ruby on Rails plugin to map any kind of ActiveRecord
+# model to a Topic Map Fragment, providing the ability
+# to expose them TMExplore compatible using REST.
+#
+# Author:: Daniel Exner
+# Copyright:: Copyright (c) 2010 Daniel Exner
+# License:: MIT License (http://www.opensource.org/licenses/mit-license.php)
+
 module ARNotations
   module Characteristics
     include LibXML
